d0cfb21d4052351c78d8842851371ececec6ff69
Terry Fadz <terry.fadz@thirdwavellc.com>
2022-03-23 11:33:54 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 // npm install gulp sass gulp-csso gulp-concat gulp-sourcemaps browser-sync
+
 const { src, dest, watch } = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
-const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
 function css() {
@@ -13,9 +16,9 @@ function css() {
   .pipe(sourcemaps.init())
   .pipe(sass())
   .pipe(autoprefixer({
-   overrideBrowserslist: ['last 2 versions'],
-   cascade: false
- }))
+    overrideBrowserslist: ['last 2 versions'],
+    cascade: false
+  }))
   .pipe(minifyCSS())
   .pipe(sourcemaps.write())
   .pipe(dest('./'))
@@ -23,9 +26,11 @@ function css() {
 }
 
 function js() {
-  return src('js/theme/*.js')
-  .pipe(concat('theme.js'))
-  .pipe(dest('js'));
+  return src('./js/theme/*.js')
+  .pipe(concat('theme.js'))   
+  .pipe(uglify())                  
+  .pipe(gulp.dest('js'))
+  .pipe(browserSync.stream());
 }
 
 function browser() {
@@ -38,7 +43,7 @@ function browser() {
   });
 
   watch('sass/**/*.scss', css);
-  watch('js/theme/*.js', js).on('change', browserSync.reload);
+  watch('js/theme/*.js', js);
 }
 
 exports.css = css;
