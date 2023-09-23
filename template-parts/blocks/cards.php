6b75dbd05f28fee1e9774ae22ccb624ba6c7f5cb
Terry Fadz <terry.fadz@thirdwavellc.com>
2023-09-23 09:20:21 -0500

    added icon selection for cards

--- template-parts/blocks/cards.php
+++ template-parts/blocks/cards.php
@@ -7,6 +7,21 @@
                 <a href="<?php if($link) : ?><?php echo $link['url'] ?><?php endif; ?>" class="card">
                     <div>
                         <div class="card-icon"><?php echo $icon; ?></div>
+                        <?php $icon = get_sub_field('select_icon'); ?>
+                        <?php if ($icon == 'globe') : ?>
+                            <i class="fas fa-globe"></i>
+                        <?php elseif ($icon == 'document') : ?>
+                            <i class="far fa-file"></i>
+                        <?php elseif ($icon == 'arrow') : ?>
+                            <i class="fas fa-chevron-right"></i>
+                        <?php elseif ($icon == 'video') : ?>
+                            <i class="fas fa-video"></i>
+                        <?php elseif ($icon == 'people') : ?>
+                            <i class="fas fa-users"></i>
+                        <?php elseif ($icon == 'person') : ?>
+                            <i class="fas fa-user"></i>
+                            
+                        <?php endif; ?>
                         <div class="subhead"><?php the_sub_field('subhead') ?></div>
                         <h3><?php the_sub_field('title') ?></h3>
                     </div>
