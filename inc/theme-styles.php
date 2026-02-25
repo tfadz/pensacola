03e2453883eae055920e6b239dc23d925e867b17
tfadz27 <alaterry@gmail.com>
2026-02-25 16:30:27 -0600

    fixed theme styles, a single s was added accidentally to the function

--- inc/theme-styles.php
+++ inc/theme-styles.php
@@ -35,7 +35,7 @@ add_action('after_setup_theme', function () {
     add_theme_support('wp-block-styles');
     add_theme_support('editor-styles');
     add_theme_support('align-wide');
-});s
+});
 
 
 
