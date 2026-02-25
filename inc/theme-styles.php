82bda9742ac9cbfbe0c28a7f21060d33fa471c29
tfadz27 <alaterry@gmail.com>
2026-02-25 16:27:40 -0600

    small change for testing

--- inc/theme-styles.php
+++ inc/theme-styles.php
@@ -14,7 +14,6 @@ function pensacola_scripts() {
     wp_enqueue_script( 'pensacola-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
     wp_enqueue_script( 'pensacola-scripts', get_template_directory_uri() . '/js/theme.js', array( 'jquery' ), '2', true );
 
-
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
